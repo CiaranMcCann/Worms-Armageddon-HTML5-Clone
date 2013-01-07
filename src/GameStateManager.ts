6d8e73f645a0d3ba887ea4878d899caaf537cd2e
Ciaran McCann <ciaran@flax.ie>
2013-01-07 20:34:44 +0000

    Taclking the mess that is GameStateManagement. With comments oh yea ha. Working out what stats are needed and what events case actions that require those states. So many edgecases </rant>

---
+++ src/GameStateManager.ts
@@ -0,0 +1,40 @@
+/**
+ * GameStateManager.js
+ *
+ *  License: Apache 2.0
+ *  author:  Ciarán McCann
+ *  url: http://www.ciaranmccann.me/
+ */
+///<reference path="system/Camera.ts"/>
+///<reference path="system/Graphics.ts"/>
+///<reference path="system/AssetManager.ts"/>
+///<reference path="system/Physics.ts"/>
+///<reference path="Worm.ts"/>
+///<reference path="system/Utilies.ts"/>
+///<reference path="system/Timer.ts" />
+///<reference path="Settings.ts" />
+
+class GameStateManager
+{
+    nextTurnTrigger: bool;
+
+    readyForNextTurn()
+    {
+
+        // REQUIRED STATES 
+        // animations finished
+        // deaths if any
+        // players health reduced if any
+        // all players most be stationary.
+
+        // EVENTS which tigger next go
+        // firing of player weapon in some cases
+        // Using up the allow shots/use of a weapon in a turn.
+        // player hurting themsleves
+        // turn time up 
+        
+        
+
+    }
+
+}
