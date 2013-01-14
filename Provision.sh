0599eac884bb561b75bcd4e354f1d3403a91cb25
Ciaran McCann <ciaran@flax.ie>
2013-01-14 17:53:46 +0000

    Small ubuntu server provisioing script so that I can deploy the games server insfructure easily if I need to CIAR-93

---
+++ Provision.sh
@@ -0,0 +1,8 @@
+sudo apt-get install git
+sudo apt-get install python-software-properties -y
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs npm -y
+sudo apt-get install upstart
+sudo apt-get install bmon
