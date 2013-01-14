995f14207d075a7e42b0cdd931562373afffd201
Ciaran McCann <ciaran@flax.ie>
2013-01-14 17:22:32 +0000

    Implemented a NetworkTimer which inherts my default timer and allows game times to be synced across clients CIAR-93

---
+++ src/networking/NetworkTimer.ts
@@ -0,0 +1,47 @@
+/**
+ * NetworkedTimer.js
+ *
+ *  License: Apache 2.0
+ *  author:  Ciarán McCann
+ *  url: http://www.ciaranmccann.me/
+ */
+///<reference path="../system/Timer.ts"/>
+///<reference path="Events.ts"/>
+///<reference path="Client.ts"/>
+
+class NetworkTimer extends Timer
+{
+
+    currentServerTime : number; // When last checked
+    packetRateTimer: Timer;
+
+    constructor(gameTurnTimeDuraction)
+    {
+        super(gameTurnTimeDuraction);
+        this.packetRateTimer = new Timer(500);
+        this.currentServerTime = Date.now();
+    }
+
+    update()
+    {
+        this.packetRateTimer.update();
+        super.update();
+
+        if (this.packetRateTimer.hasTimePeriodPassed())
+        {
+            Client.socket.emit(Events.client.GET_GAME_TIME, '',function (data) =>
+            {
+                console.log(data);
+                this.currentServerTime = data;
+            });
+        }
+    }
+    
+
+    //override
+    getTimeNow()
+    {             
+        return this.currentServerTime;
+    }
+
+}
