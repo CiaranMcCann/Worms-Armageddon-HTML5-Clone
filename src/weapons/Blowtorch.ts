d48f7ed534155d1134ae0128e898a2c1aa9e4471
Ciaran McCann <ciaran@flax.ie>
2013-03-03 19:04:29 +0000

    Inital blowtorch code CIAR-122

---
+++ src/weapons/Blowtorch.ts
@@ -0,0 +1,29 @@
+/**
+ * Blowtorch.js
+ *
+ *  License: Apache 2.0
+ *  author:  Ciarán McCann
+ *  url: http://www.ciaranmccann.me/
+ */
+///<reference path="../system/Physics.ts"/>
+///<reference path="../system/Utilies.ts" />
+///<reference path="../Worm.ts" />
+///<reference path="../animation/Sprite.ts"/>
+///<reference path="../system/Timer.ts"/>
+///<reference path="../Game.ts"/>
+///<reference path="BaseWeapon.ts"/>
+///<reference path="Drill.ts"/>
+
+class Blowtorch extends Drill
+{
+    constructor(ammo)
+    {
+        super(ammo,
+            "Blowtorch",
+            Sprites.weaponIcons.blowTorch,
+            Sprites.worms.takeOutBlowtorch,
+            Sprites.worms.blowtorching
+        )
+    }
+
+}
